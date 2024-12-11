85dc1c7fa20a38da9cd05024bca0b09333eb3dbb
allae <ibrahimallae1@gmail.com>
2024-12-11 13:47:10 +0100

    Ajout du signup

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-
 const app = express();
+
+// Middleware pour permettre les CORS (Cross-Origin Resource Sharing)
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
-  });
+});
 
+// Middleware pour analyser le corps des requêtes en JSON
+app.use(express.json());
+
+// Route GET pour récupérer des objets
 app.use('/api/stuff', (req, res, next) => {
     const stuff = [
       {
@@ -20,6 +25,15 @@ app.use('/api/stuff', (req, res, next) => {
       },
     ];
     res.status(200).json(stuff);
-  });
+});
+
+// Route POST pour le signup
+app.post("/api/auth/signup", signup);
+
+function signup(req, res) {
+    const body = req.body; // Récupération du corps de la requête
+    console.log("body:", body);
+    res.status(200).json({ message: "Login successful", data: body });
+}
 
 module.exports = app;
